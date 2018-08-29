77c4007f2294c1eaf1b0fc02c13fa162b0856d7c
Rhisiart ap Gwilym <rhisiart@hiafi.co.uk>
2018-08-29 11:58:36 +0200

    Add some buttons to sample-app to help visualise the difference between the animated and non-animated open/close functions

--- sample-app/src/main/java/android/support/widget/MainActivity.java
+++ sample-app/src/main/java/android/support/widget/MainActivity.java
@@ -2,13 +2,34 @@ package android.support.widget;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main_activity);
-	}
-	
+    private SlidingDrawer sd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        sd = findViewById(R.id.slidingDrawer);
+    }
+
+    public void onOpen(View view) {
+        sd.setsetDrawerOpen(false);
+    }
+
+    public void onOpenAnimated(View view) {
+        sd.setsetDrawerOpen(true);
+    }
+
+    public void onClose(View view) {
+        sd.setDrawerClosed(false);
+    }
+
+    public void onCloseAnimated(View view) {
+        sd.setDrawerClosed(true);
+    }
+
 }
