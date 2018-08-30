a5d6fc85c911818b153652172df63dfce1a96407
Rhisiart ap Gwilym <rhisiart@hiafi.co.uk>
2018-08-30 08:58:03 +0200

    Add example layouts to sample-app

--- sample-app/src/main/java/android/support/widget/MainActivity.java
+++ sample-app/src/main/java/android/support/widget/MainActivity.java
@@ -3,33 +3,40 @@ package android.support.widget;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    private SlidingDrawer sd;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private SlidingDrawer slidingDrawer;
+    private int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        sd = findViewById(R.id.slidingDrawer);
+        slidingDrawer = findViewById(R.id.slidingDrawer);
     }
 
     public void onOpen(View view) {
-        sd.setsetDrawerOpen(false);
+        slidingDrawer.setsetDrawerOpen(false);
     }
 
     public void onOpenAnimated(View view) {
-        sd.setsetDrawerOpen(true);
+        slidingDrawer.setsetDrawerOpen(true);
     }
 
     public void onClose(View view) {
-        sd.setDrawerClosed(false);
+        slidingDrawer.setDrawerClosed(false);
     }
 
     public void onCloseAnimated(View view) {
-        sd.setDrawerClosed(true);
+        slidingDrawer.setDrawerClosed(true);
     }
 
+    public void onTextViewClick(View view) {
+        TextView tv = findViewById(R.id.interactive_widget);
+        tv.setText(String.valueOf(++count));
+    }
 }
